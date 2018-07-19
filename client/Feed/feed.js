e4ae85143471169fcb574bca2c1d7eefaaa75816
Fernanda Novato <fernandagabriela1997@gmail.com>
2018-07-19 01:42:17 -0300

    Cabeçalho e página inicial

--- client/Feed/feed.js
+++ client/Feed/feed.js
@@ -1,7 +1,8 @@
 
 Template.Feed.helpers({
 	posts: function() {
-		var postsDaCollection = Posts.find().fetch();
+		var postsDaCollection = Posts.find().fetch().reverse;
+		//Inverter a ordem dos posts, pros mais novos aparecem no topo
 		return postsDaCollection;
 	}
 });
